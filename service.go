ad7e8190c32f79f809dc0f4ef7699a4791b38966
shellfly <shell0fly@gmail.com>
2022-12-28 15:41:36 +0800

    feat: execQuery

--- service.go
+++ service.go
@@ -43,16 +43,18 @@ func NewService() *Service {
 }
 
 func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	table := strings.Trim(r.URL.Path, "/")
+
 	var res Response
 	switch r.Method {
 	case "POST":
-		res = s.create(w, r)
+		res = s.create(r, table)
 	case "DELETE":
-		res = s.delete(w, r)
+		res = s.delete(r, table)
 	case "PUT":
-		res = s.update(w, r)
+		res = s.update(r, table)
 	case "GET":
-		res = s.get(w, r)
+		res = s.get(r, table)
 	default:
 		if _, err := w.Write([]byte(fmt.Sprintf("method not supported: %s", r.Method))); err != nil {
 			log.Fatal(err)
@@ -61,13 +63,11 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(res); err != nil {
-		log.Printf("failed to encode json data, err: %v", err)
+		log.Printf("failed to encode json data, %v", err)
 	}
 }
 
-func (s *Service) create(w http.ResponseWriter, r *http.Request) Response {
-	table := strings.Trim(r.URL.Path, "/")
-
+func (s *Service) create(r *http.Request, tableName string) Response {
 	data := make(map[string]any)
 	// Try to decode the request body into the struct. If there is an error,
 	// respond to the client with the error message and a 400 status code.
@@ -75,13 +75,17 @@ func (s *Service) create(w http.ResponseWriter, r *http.Request) Response {
 	if err != nil {
 		return Response{
 			Code: http.StatusBadRequest,
-			Msg:  fmt.Sprintf("failed to parse http body as json data, err: %v", err),
+			Msg:  fmt.Sprintf("failed to parse http body as json data, %v", err),
+		}
+	}
+	if len(data) == 0 {
+		return Response{
+			Code: http.StatusBadRequest,
+			Msg:  "empty data",
 		}
 	}
 
 	log.Printf("get json data: %+v", data)
-	ctx, cancel := context.WithTimeout(r.Context(), 120*time.Second)
-	defer cancel()
 	placeholders := make([]string, 0, len(data))
 	names := make([]string, 0, len(data))
 	args := make([]any, 0, len(data))
@@ -93,22 +97,12 @@ func (s *Service) create(w http.ResponseWriter, r *http.Request) Response {
 		index++
 	}
 
-	result, err := s.db.ExecContext(ctx,
-		fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", table, strings.Join(names, ","), strings.Join(placeholders, ",")),
-		args...,
-	)
-	if err != nil {
-
-		return Response{
-			Code: http.StatusInternalServerError,
-			Msg:  fmt.Sprintf("failed to insert data to database, err: %v", err),
-		}
-	}
-	rows, err := result.RowsAffected()
+	sql := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", tableName, strings.Join(names, ","), strings.Join(placeholders, ","))
+	rows, err := s.execQuery(r.Context(), sql, args)
 	if err != nil {
 		return Response{
 			Code: http.StatusInternalServerError,
-			Msg:  fmt.Sprintf("failed to parse  database respons, err: %v", err),
+			Msg:  err.Error(),
 		}
 	}
 	if rows != 1 {
@@ -124,21 +118,75 @@ func (s *Service) create(w http.ResponseWriter, r *http.Request) Response {
 	}
 }
 
-func (s *Service) delete(w http.ResponseWriter, r *http.Request) Response {
+func (s *Service) delete(r *http.Request, tableName string) Response {
+	sql := fmt.Sprintf("DELETE FROM %s", tableName)
+	_, query, args := buildWhereQuery(1, r.URL.Query())
+	if len(query) > 0 {
+		sql += fmt.Sprintf(" WHERE %s", query)
+	}
+
+	_, err := s.fetchData(r.Context(), sql, args...)
+	if err != nil {
+		return Response{
+			Code: http.StatusInternalServerError,
+			Msg:  err.Error(),
+		}
+	}
+
 	return Response{
 		Code: http.StatusOK,
 		Msg:  "success",
 	}
 }
 
-func (s *Service) update(w http.ResponseWriter, r *http.Request) Response {
+func (s *Service) update(r *http.Request, tableName string) Response {
+	var sqlBuilder strings.Builder
+	sqlBuilder.WriteString("UPDATE ")
+	sqlBuilder.WriteString(tableName)
+
+	data := make(map[string]any)
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		return Response{
+			Code: http.StatusBadRequest,
+			Msg:  fmt.Sprintf("failed to parse http body as json data, %v", err),
+		}
+	}
+	if len(data) == 0 {
+		return Response{
+			Code: http.StatusBadRequest,
+			Msg:  "empty data",
+		}
+	}
+	log.Printf("get json data: %+v", data)
+
+	index, setQuery, args := buildSetQuery(1, data)
+	sqlBuilder.WriteString(" SET ")
+	sqlBuilder.WriteString(setQuery)
+
+	_, query, args2 := buildWhereQuery(index, r.URL.Query())
+	if len(query) > 0 {
+		sqlBuilder.WriteString(" WHERE ")
+		sqlBuilder.WriteString(query)
+		args = append(args, args2...)
+	}
+
+	rows, err := s.execQuery(r.Context(), sqlBuilder.String(), args)
+	if err != nil {
+		return Response{
+			Code: http.StatusInternalServerError,
+			Msg:  err.Error(),
+		}
+	}
 	return Response{
 		Code: http.StatusOK,
-		Msg:  "success",
+		Msg:  fmt.Sprintf("successfully updated %d rows", rows),
 	}
 }
 
-func (s *Service) get(w http.ResponseWriter, r *http.Request) Response {
+func (s *Service) get(r *http.Request, tableName string) Response {
 	// Example
 	// fetch all:
 	//   http get "localhost:8080/artists"
@@ -147,57 +195,15 @@ func (s *Service) get(w http.ResponseWriter, r *http.Request) Response {
 	// query by array:
 	//   http get "localhost:8080/artists?ArtistId=1&ArtistId=2"
 
-	tableName := strings.Trim(r.URL.Path, "/")
-	// TODO: validate table exists in database
 	sql := fmt.Sprintf("SELECT * FROM %s", tableName)
-
-	query := r.URL.Query()
-	args := make([]any, 0, len(query))
+	_, query, args := buildWhereQuery(1, r.URL.Query())
 	if len(query) > 0 {
-		// build where clause
-		var queryBuilder strings.Builder
-		count := 0
-		index := 1
-		for k, v := range query {
-			v := filterEmpty(v)
-			if len(v) == 0 {
-				log.Printf("empty query: %s\n", k)
-				continue
-			}
-			if count > 0 {
-				queryBuilder.WriteString(" AND ")
-			}
-			queryBuilder.WriteString(k)
-			if len(v) == 1 {
-				queryBuilder.WriteString(" = ")
-				queryBuilder.WriteString(fmt.Sprintf("$%d", index))
-				args = append(args, v[0])
-				index++
-			} else {
-				queryBuilder.WriteString(" in (")
-				for i, vv := range v {
-					queryBuilder.WriteString(fmt.Sprintf("$%d", index))
-					if i != len(v)-1 {
-						queryBuilder.WriteString(",")
-					}
-					args = append(args, vv)
-					index++
-				}
-				queryBuilder.WriteString(")")
-
-			}
-			count++
-		}
-		s := queryBuilder.String()
-		if len(s) > 0 {
-			sql += fmt.Sprintf(" WHERE %s", s)
-		}
+		sql += fmt.Sprintf(" WHERE %s", query)
 	}
 
 	// TODO: handle page operations
 	sql += " LIMIT 10"
 
-	log.Printf("sql: %v, args: %v", sql, args)
 	objects, err := s.fetchData(r.Context(), sql, args...)
 	if err != nil {
 		return Response{
@@ -213,19 +219,38 @@ func (s *Service) get(w http.ResponseWriter, r *http.Request) Response {
 	}
 }
 
+// execQuery ...
+func (s *Service) execQuery(ctx context.Context, sql string, args ...any) (int64, error) {
+	log.Printf("exec query in database, sql: %v, args: %v", sql, args)
+	ctx, cancel := context.WithTimeout(ctx, 120*time.Second)
+	defer cancel()
+
+	result, err := s.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return 0, fmt.Errorf("failed to insert data to database, %w", err)
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse database rows, %w", err)
+	}
+	return rows, nil
+}
+
+// fetchData ...
 func (s *Service) fetchData(ctx context.Context, sql string, args ...any) ([]any, error) {
+	log.Printf("fetch data in database, sql: %v, args: %v", sql, args)
 	ctx, cancel := context.WithTimeout(ctx, 120*time.Second)
 	defer cancel()
 
 	rows, err := s.db.QueryContext(ctx, sql, args...)
 	if err != nil {
-		return nil, fmt.Errorf("failed to run query in database, err: %w", err)
+		return nil, fmt.Errorf("failed to run query in database, %w", err)
 	}
 	defer rows.Close()
 
 	columnTypes, err := rows.ColumnTypes()
 	if err != nil {
-		return nil, fmt.Errorf("failed to get columns from database, err: %v", err)
+		return nil, fmt.Errorf("failed to get columns from database, %v", err)
 	}
 
 	count := len(columnTypes)
@@ -246,7 +271,7 @@ func (s *Service) fetchData(ctx context.Context, sql string, args ...any) ([]any
 
 		err := rows.Scan(scanArgs...)
 		if err != nil {
-			return nil, fmt.Errorf("failed to scan data from database, err: %v", err)
+			return nil, fmt.Errorf("failed to scan data from database, %v", err)
 		}
 
 		object := make(map[string]any, count)
@@ -256,18 +281,8 @@ func (s *Service) fetchData(ctx context.Context, sql string, args ...any) ([]any
 		objects = append(objects, object)
 	}
 	if err = rows.Err(); err != nil {
-		return nil, fmt.Errorf("failed to fetch rows from database, err: %v", err)
+		return nil, fmt.Errorf("failed to fetch rows from database, %v", err)
 
 	}
 	return objects, nil
-}
-
-func filterEmpty(in []string) []string {
-	out := make([]string, 0, len(in))
-	for _, s := range in {
-		if s != "" {
-			out = append(out, s)
-		}
-	}
-	return out
 }
---
+++ utils.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"strings"
+)
+
+func filterEmpty(in []string) []string {
+	out := make([]string, 0, len(in))
+	for _, s := range in {
+		if s != "" {
+			out = append(out, s)
+		}
+	}
+	return out
+}
+
+// buildWhereQuery build sql where clause from url values
+func buildWhereQuery(index int, query url.Values) (int, string, []any) {
+	if len(query) == 0 {
+		return index, "", nil
+	}
+
+	var queryBuilder strings.Builder
+	args := make([]any, 0, len(query))
+	count := 0
+	for k, v := range query {
+		v := filterEmpty(v)
+		if len(v) == 0 {
+			log.Printf("empty query: %s\n", k)
+			continue
+		}
+		if count > 0 {
+			queryBuilder.WriteString(" AND ")
+		}
+		queryBuilder.WriteString(k)
+		if len(v) == 1 {
+			queryBuilder.WriteString(" = ")
+			queryBuilder.WriteString(fmt.Sprintf("$%d", index))
+			args = append(args, v[0])
+			index++
+		} else {
+			queryBuilder.WriteString(" in (")
+			for i, vv := range v {
+				queryBuilder.WriteString(fmt.Sprintf("$%d", index))
+				if i != len(v)-1 {
+					queryBuilder.WriteString(",")
+				}
+				args = append(args, vv)
+				index++
+			}
+			queryBuilder.WriteString(")")
+		}
+		count++
+	}
+
+	return index, queryBuilder.String(), args
+}
+
+func buildSetQuery(index int, data map[string]any) (int, string, []any) {
+	if len(data) == 0 {
+		return index, "", nil
+	}
+
+	var queryBuilder strings.Builder
+	args := make([]any, 0, len(data))
+	for k, v := range data {
+		queryBuilder.WriteString(k)
+		queryBuilder.WriteString(" = ")
+		queryBuilder.WriteString(fmt.Sprintf("$%d", index))
+		args = append(args, v)
+		index++
+	}
+
+	return index, queryBuilder.String(), args
+}
